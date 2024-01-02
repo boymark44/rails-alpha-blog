56e4b5bc0830de27aac6142f36015922e2870cfb
Mark Glenn M. Gabuya <markglenn.g@gmail.com>
2024-01-02 16:50:21 +0800

    Added the model articles to our app.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,5 @@
 class Article < ApplicationRecord
-
+  # Adding validations to our article model
+  validates :title, presence: true, length: {minimum: 6, maximum: 100}
+  validates :description, presence: true, length: {minimum: 10, maximum: 300}
 end
