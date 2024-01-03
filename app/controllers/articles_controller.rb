ebeb1d4585dcd524574fc54c6a2cc97da89063a1
Mark Glenn M. Gabuya <markglenn.g@gmail.com>
2024-01-03 15:26:28 +0800

    Add the new, create, edit, and update action in our articles controller.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,5 +10,46 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  # Display the form to create a new article.
+  def new
+    @article = Article.new
+  end
+
+    # The edit action is used to display the form to edit an existing article.
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+
+  # Create a new article.
+  def create
+    #@article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:notice] = "Article was created, successfully."
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+  end
+
+  # The update action is used to update an existing article.
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was updated, successfully."
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  # The article_params method is used to whitelist the parameters that are passed to the create and update actions.
+  def article_params
+    params.require(:article).permit(:title, :description) # Whitelist the parameters.
+  end
+
 
 end
