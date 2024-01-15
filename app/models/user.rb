1490359f9b215b0638529c15ec3c1c815fbef6e5
Mark Glenn M. Gabuya <markglenn.g@gmail.com>
2024-01-15 15:37:49 +0800

    Alter the object state using the before_save method to transform all the emails into lowercase.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
+  # Add the before_save callback to downcase the email
+  before_save { self.email = email.downcase }
+
   # Add the one_to_many relationship
   has_many :articles
 
+  # Add the validations to the model attributes (username, email)
   validates :username, presence: true, uniqueness: true, length: {minimum: 3, maximum: 25}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: true, length: {maximum: 105}, format: {with: VALID_EMAIL_REGEX}
